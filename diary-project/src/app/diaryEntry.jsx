b851b1aea286e62b74a645e1b08e3ebe2ed1e00c
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-29 17:30:31 -0500

    styles

--- diary-project/src/app/diaryEntry.jsx
+++ diary-project/src/app/diaryEntry.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
+
 export default function DiaryEntry () {
+    
+    const [entry, setEntry] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        setEntry('');
+  }
+
   return (
     <div className='form' style={{ width: '80%', margin: '0 auto' }}>
-      <form className='entry' style={{ width: '100%' }}>
+      <form className='entry' style={{ width: '90%' }}>
         
-        <input type='text' placeholder='Enter a diary entry...' />
+        <input type='text' placeholder='Enter a diary entry...'
+        value={entry}
+        onChange={(e) => setEntry(e.target.value)}
+        />
       
       </form>
 
-      <button className='submit'>Submit</button>
+      <button className='submit' type="submit">Submit</button>
     </div>
   )
 }
