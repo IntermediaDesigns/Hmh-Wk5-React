a7adc11842bef862f4fe46705645b5e48a57add7
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-29 16:55:19 -0500

    styling

--- diary-project/src/app/days.jsx
+++ diary-project/src/app/days.jsx
@@ -7,7 +7,7 @@ export default function Days() {
     return (
         <>
         
-        <div className="daysContainer">
+        <div className="daysContainer" style={{width: '95%', margin: '20px auto'}}>
             
             
             
--- diary-project/src/app/diaryEntry.jsx
+++ diary-project/src/app/diaryEntry.jsx
@@ -1,8 +1,13 @@
-export default function DiaryEntry() {
-    return (
-        <div className="form" style={{width: '80%', margin: '0 auto'}}>
-        <form className="entry" style={{width: '100%'}}>Text</form>
-        <button className="submit">Submit</button>
-        </div>
-    );
+export default function DiaryEntry () {
+  return (
+    <div className='form' style={{ width: '80%', margin: '0 auto' }}>
+      <form className='entry' style={{ width: '100%' }}>
+        
+        <input type='text' placeholder='Enter a diary entry...' />
+      
+      </form>
+
+      <button className='submit'>Submit</button>
+    </div>
+  )
 }
