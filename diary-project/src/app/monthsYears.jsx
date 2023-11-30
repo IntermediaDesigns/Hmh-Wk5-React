1366a9e66a2b22af010a1a90e7c737c1533818c3
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-30 14:46:23 -0500

    adding notes

--- diary-project/src/app/monthsYears.jsx
+++ diary-project/src/app/monthsYears.jsx
@@ -1,5 +1,7 @@
+// using useState to manage the state of months, years, currentMonthIndex, and currentYearIndex.
 import { useState } from 'react'
 
+// two props: onMonthChange and onYearChange, they are callback functions that will be called when the month or year changes.
 export default function MonthYear ({ onMonthChange, onYearChange }) {
   const [months, setMonth] = useState([
     { id: 1, month: 'January' },
@@ -32,7 +34,7 @@ export default function MonthYear ({ onMonthChange, onYearChange }) {
     years.findIndex(y => y.year === currentDate.getFullYear())
   )
 
-  // Function to handle month change
+  // Function to handle month change, it increments or decrements the currentMonthIndex. If the new index is out of range, it wraps around to the other end of the array and changes the year. After updating the index, it calls the onMonthChange callback with the new index.
   const handleMonthChange = direction => {
     let newIndex =
         direction === 'next' ? currentMonthIndex + 1 : currentMonthIndex - 1;
@@ -47,7 +49,7 @@ export default function MonthYear ({ onMonthChange, onYearChange }) {
     onMonthChange && onMonthChange(newIndex);
 };
 
-// Function to handle year change
+// Function to handle year change, does about same for month but in year
 const handleYearChange = direction => {
     let newIndex =
         direction === 'next' ? currentYearIndex + 1 : currentYearIndex - 1;
@@ -61,7 +63,7 @@ const handleYearChange = direction => {
 };
 
   
-
+// two child divs, one for the month and one for the year. Each child div has two buttons for changing the month/year and a div to display the current month/year. The onClick prop of the buttons is set to call handleMonthChange or handleYearChange when clicked.
   return (
     <div className='monthYearContainer'>
       <div className='mthContainer'>
